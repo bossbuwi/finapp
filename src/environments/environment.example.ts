6d3ced43171e22f2003dcf681b506e0dfb6f813a
bossbuwi <paradoxhorizondevs@gmail.com>
2026-03-11 20:12:23 +0800

    Add enviroment template

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  supabaseUrl: 'https://hyefyyzumwfaxlcllpeo.supabase.co',
-  supabaseKey: 'sb_publishable_6-uWuZZ7MTehnL0N1ej1vQ_hCoNkBND'
+  supabaseUrl: '',
+  supabaseKey: ''
 };
